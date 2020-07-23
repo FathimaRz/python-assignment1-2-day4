23b91aad91446018f2aef569b6d4cb13b495885f
FathimaRz <68552053+FathimaRz@users.noreply.github.com>
2020-07-23 19:31:27 +0530

    Add files via upload

---
+++ ASSIGNMENT1DAY4RAZ.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+str1= "what we think we become; we are python programmers"
+str1.count ("we")
+
+
+# In[3]:
+
+
+str1.count ("we",0,15)
+
+
+# In[4]:
+
+
+str1[0:15].count("we")
+
+
+# In[5]:
+
+
+str1.find("we")
+
+
+# In[6]:
+
+
+str1[5]
+
+
+# In[7]:
+
+
+str1.rfind("we")
+
+
+# In[8]:
+
+
+str1.index("we")
+
+
+# In[ ]:
+
+
+
+
---
+++ assignment2day4raz (1).py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+str1="GALAXY"
+print (str1.isupper())
+
+
+# In[2]:
+
+
+str2="galaxy"
+print (str2.isupper())
+
+
+# In[4]:
+
+
+str2="galaxy"
+print (str2.islower())
+
+
+# In[6]:
+
+
+str1="GALAXY"
+print (str1.islower())
+
+
+# In[8]:
+
+
+str1="GALAXY"
+print (str1.lower())
+
+
+# In[9]:
+
+
+str2="Galaxy"
+print (str2.lower())
+
+
+# In[10]:
+
+
+str2="galaxy"
+print (str2.upper())
+
+
+# In[11]:
+
+
+str4="my name is fathima razzana"
+print (str4.upper())
+
+
+# In[ ]:
+
+
+a=input("enter the value")
+if a.isupper():
+    a=eval(a)
+print (a,type(a))
+
+
+# In[ ]:
+
+
+
+
